6f68f279e78c532ff330c73ac706961afa95b7b2
unknown <mateusdsillva@gmail.com>
2020-08-09 15:32:29 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+
+/* TESTE DE COMANDOS E TAGS, JS - AULA DA DIO */
+/*
+
+function trocar() {
+    document.getElementById("msv").innerHTML = "Obrigado";
+}
+
+
+*/
+
+
+
+
+
+/*
+
+function cl1(){
+document.getElementById("m1").innerHTML = "<p>Olá</p><button>button 1</button><button>button 1</button><input type='text'>";
+
+}*/
+   /* alert("Uhuu");
+}
+
+
+*/
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+alert(soma(5, 10));*/
+/*var d = new Date();
+alert(d); */
+
+
+/*var count = 5;
+
+while (count < 5){
+    console.log(count);
+
+    alert(count);
+    count++;
+
+};
+*/
+
+/*var nome ="Mateus";
+var idade = prompt("Qaul é sua idade?")
+
+if (idade > 18) {
+    alert("Maior de Idade");} else {
+        alert("Menor de Idade")
+    };*/
+//var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxo"}];
+//alert(frutas);
+
+//var lista = ["maça","uva", "arroz"];
+/* lista.push("pêra"); */
+//console.log(lista.join(" - "));
+
+/*alert(nome + " tem " + idade + " anos ")*/
+/*console.log(lista)*/
+/* lista.push("pêra"); */
+/* Comentário */
+/*console.log(lista.reverse);*/
